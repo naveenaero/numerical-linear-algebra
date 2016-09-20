506d9637a3be566f0ffeb160446575ed95b1429f
Naveen Himthani <naveen@orion.ices.utexas.edu>
2016-09-20 16:28:02 -0500

    Classical Gram Schmidt first version

---
+++ gramschmidt.m
@@ -0,0 +1,17 @@
+function [ Q,R ] = gramschmidt( A, flag )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+r = size(A);
+Q = zeros(r);
+R = zeros(r(2));
+
+if flag
+    for j=1:n
+        W = A(:,j)
+    display('Hello')
+else
+    
+
+end
+
